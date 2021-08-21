d04ccaecc08f7fc86a99427576848aa3f90a4e46
Ahmedansari7255 <82463481+Ahmedansari7255@users.noreply.github.com>
2021-08-21 09:21:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let parentEl=document.getElementById("parent");
+console.log(parentEl);
+
+console.log(parentEl.firstChild);
+
+console.log(parentEl.lastChild);
+
+console.log(parentEl.childNodes);
+
+console.log(parentEl.firstElementChild);
+
+console.log(parentEl.lastElementChild);
+
+console.log(parentEl.children[3]);
+
+let midEl=document.getElementById("middle");
+console.log(middle);
+
+console.log(midEl.nextElementSibling);
+
+console.log(midEl.previousElementSibling);
+
+let pEl=document.getElementById("middle");
+console.log(pEl.parentElement);
+
+
+
