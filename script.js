5b35410e256598ecd93c1746e4a15dca646a84ec
Ahmedansari7255 <82463481+Ahmedansari7255@users.noreply.github.com>
2021-08-21 10:47:43 +0530

    second commit

--- script.js
+++ script.js
@@ -1,27 +1,86 @@
-let parentEl=document.getElementById("parent");
-console.log(parentEl);
 
-console.log(parentEl.firstChild);
+// let parentEl=document.getElementById("parent");
+// console.log(parentEl);
 
-console.log(parentEl.lastChild);
+// console.log(parentEl.firstChild);
 
-console.log(parentEl.childNodes);
+// console.log(parentEl.lastChild);
 
-console.log(parentEl.firstElementChild);
+// console.log(parentEl.childNodes);
 
-console.log(parentEl.lastElementChild);
+// console.log(parentEl.firstElementChild);
 
-console.log(parentEl.children[3]);
+// console.log(parentEl.lastElementChild);
 
-let midEl=document.getElementById("middle");
-console.log(middle);
+// console.log(parentEl.children[3]);
 
-console.log(midEl.nextElementSibling);
+// let midEl=document.getElementById("middle");
+// console.log(middle);
 
-console.log(midEl.previousElementSibling);
+// console.log(midEl.nextElementSibling);
 
-let pEl=document.getElementById("middle");
-console.log(pEl.parentElement);
+// console.log(midEl.previousElementSibling);
+
+// let pEl=document.getElementById("middle");
+// console.log(pEl.parentElement);
+
+//applying individually
+let para=document.getElementById("access");
+console.log(para);
+para.style.backgroundColor="red";
+para.style.color="white";
+
+//applying in same line using csstext
+para.style.cssText="color:white background-color:red";
+
+//using classnames
+para.classList.add("textColor");
+
+//
+function onButtonClick (){
+    let para=document.getElementById("access");
+    para.classList.add("textColor");
+    console.log("button clicked");
+}
+
+//
+
+function onButtonClick (){
+    let para=document.getElementById("access");
+    para.classList.add("textColor");
+}
 
 
+function onButtonClick2 (){
+    let para=document.getElementById("access");
+    para.classList.remove("textColor");
+    
+}
 
+//inner text
+let para=document.getElementById("access");
+console.log(para.innerText);
+
+//text content
+let para=document.getElementById("access");
+console.log(para.textContent);
+
+//inner html
+let para=document.getElementById("access");
+console.log(para.innerHTML);
+
+//
+let para=document.getElementById("access");
+para.innerText="learning     DOM";
+
+//
+let para=document.getElementById("access");
+para.innerText="learning     <b>DOM</b>";
+
+//
+let para=document.getElementById("access");
+para.textContent="learning     <b>DOM</b>";
+
+//
+let para=document.getElementById("access");
+para.innerHTML="learning     <b>DOM</b>";
